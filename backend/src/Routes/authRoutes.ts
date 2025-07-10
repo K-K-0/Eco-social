1ba809b14b506615188e7ec18cf79e04ef6ce037
K-K-0 <aadityyadav7531@gmail.com>
2025-07-10 09:50:19 +0530

    fixed cookie token issue

--- backend/src/Routes/authRoutes.ts
+++ backend/src/Routes/authRoutes.ts
@@ -63,6 +63,7 @@ router.get('/me', authMiddleware, async (req:any, res:any) => {
         res.json({user})
     } catch (error) {
         res.status(500).json({massage: 'server error'})
+        console.log(error)
     }
 })
 
