01cc22956128a3224d7b8526de07f4fe4a979e4a
K-K-0 <aadityyadav7531@gmail.com>
2025-07-14 11:40:53 +0530

    created tree planting system

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,7 +41,7 @@ app.use(setLocation)
 
 
 
-const PORT = Number(process.env.PORT) || 8080;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server is running on port ${PORT}`);
