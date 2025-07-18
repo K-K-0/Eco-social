1015f01950a3d40fc753ee551f52015082a46426
K-K-0 <aadityyadav7531@gmail.com>
2025-07-18 13:38:00 +0530

    fixing Map errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,12 +19,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: ['https://eco-social.vercel.app',
-        'https://eco-social-k-k-0s-projects.vercel.app',
-        'https://eco-social-git-main-k-k-0s-projects.vercel.app',
-        'https://eco-social-jz0u6qw4k-k-k-0s-projects.vercel.app'
-        
-        ],
+    origin: ['http://localhost:5173/'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
