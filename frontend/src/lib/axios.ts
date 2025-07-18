062e14391199a9c081273221bce5174c210a30e4
K-K-0 <aadityyadav7531@gmail.com>
2025-07-18 12:59:43 +0530

    fixing Map errors

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // const BASE_URL = import.meta.env.VITE_BACKEND_URL
 
 const API = axios.create({
-    baseURL: `http://eco-social-production-00a1.up.railway.app/api`,
+    baseURL: `https://eco-social-production-00a1.up.railway.app/api`,
     withCredentials: true
 })
 
